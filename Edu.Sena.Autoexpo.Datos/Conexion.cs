88ba74a6a0a4e09a3c81d3cef77582d0b3232dc5
davr <davr@DESKTOP-0S26F42>
2018-09-21 16:15:31 -0500

    Add project files.

---
+++ Edu.Sena.Autoexpo.Datos/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edu.Sena.Autoexpo.Datos {
+    public class Conexion {
+        public static SqlConnection Con { get; set; } = new SqlConnection("Data Source=DESKTOP-0S26F42\\SQLEXPRESS;Initial Catalog=Autoexpo;Integrated Security=True");
+
+        public static void Abrir() {
+            try {
+                Cerrar();
+                Con.Open();
+                MessageBox.Show("¡Conexión exitosa!");
+            } catch (SqlException e) {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("ERROR: no se pudo conectar a la base de datos");
+            }
+        }
+
+        public static void Cerrar() {
+            try {
+                Con.Close();
+            } catch (SqlException e) {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("ERROR: no se pudo cerrar la conexión a la base de datos");
+            }
+        }
+    }
+}
---
+++ Edu.Sena.Autoexpo.Logica/LogicaUtil.cs
@@ -0,0 +1,15 @@
+﻿using Edu.Sena.Autoexpo.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu.Sena.Autoexpo.Logica {
+    public class LogicaUtil {
+        public static void Conectar() {
+            Conexion.Abrir();
+            Conexion.Cerrar();
+        }
+    }
+}
